ccb68466c6bb679840c287ffa8dbc2b2a37d5f42
deborabeatrizf <deborabeatriz.f@gmail.com>
2025-03-21 16:43:40 -0300

    create Main index jsx

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,23 @@
+export default function Main() {
+  return (
+    <section className="main">
+      <h2 className="main__title">Welcome to Country API!</h2>
+      <p className="main__text">
+        Looking to explore the world, one country at a time? You've come to the
+        right place!
+      </p>
+      <p className="main__text">
+        Explore the world and search information about a country you are
+        interested in traveling to or simply have curiosity about it.
+      </p>
+      <p className="main__text">
+        This simple and easy-to-use application lets you search for any country
+        and instantly discover its flag, name, and region.
+      </p>
+      <p className="main__text">
+        No hassle, type the name of a country in the search bar and start
+        exploring!
+      </p>
+    </section>
+  );
+}
