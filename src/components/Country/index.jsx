cf09c5ed9221b8869602e9604fee34e947f6645b
deborabeatrizf <deborabeatriz.f@gmail.com>
2025-03-21 21:52:54 -0300

    update country index jsx

--- src/components/Country/index.jsx
+++ src/components/Country/index.jsx
@@ -1,17 +1,57 @@
+import { useState } from "react";
+import searchCountry from "../../utils/searchCountry";
+import Preloader from "../Preloader";
+import "./country.css";
+
 export default function Country() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [input, setInput] = useState("");
+  const [countryData, setCountryData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    setError(null);
+    setCountryData(null);
+
+    try {
+      const data = await searchCountry(input);
+      setCountryData(data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+      setInput("");
+    }
+  };
+
   return (
     <>
       <div className="country">
-        <form className="country__form">
+        <form onSubmit={handleSubmit} className="country__form">
           <input
             type="text"
             placeholder="Type Country Name"
+            value={input}
+            onChange={(event) => {
+              setInput(event.target.value);
+            }}
             className="country__input"
           />
           <button type="submit" className="country__submit">
             Search
           </button>
         </form>
+        {isLoading && <Preloader />}
+        {error && <p className="country_error">{error}</p>}
+        {countryData && (
+          <div className="country__info">
+            <h2 className="country__name">{countryData.name}</h2>
+            <p className="country__capital">Capital: {countryData.capital}</p>
+            <p className="country__region">Region: {countryData.region}</p>
+          </div>
+        )}
       </div>
     </>
   );
