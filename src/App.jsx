92851220309fdcd1e294a59964772749f2ae0eb3
deborabeatrizf <deborabeatriz.f@gmail.com>
2025-03-21 22:55:25 -0300

    update app jsx - add route about

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Country from "./components/Country/index";
+import About from "./components/About";
 import "./App.css";
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/country" element={<Country />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </main>
     </div>
